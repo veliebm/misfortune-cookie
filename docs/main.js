39286a8c31c41a399e630c03e83918f180573d7f
veliebm <veliebm@gmail.com>
2022-12-04 11:42:14 -0600

    Create a misfortune when cookie you break cookie

--- docs/main.js
+++ docs/main.js
@@ -1,3 +1,9 @@
+const FORTUNES = [
+    "Abandon all hope immediately.",
+    "Misfortune favors you a whole lot.",
+    "By the time you're reading this, it's already too late.",
+];
+
 var cookie_broken = false;
 
 document.onkeydown = function (e) {
@@ -17,6 +23,7 @@ document.onkeyup = function (e) {
             document.getElementById("cookie").classList.add("invisible");
             cookie_broken = true;
             document.getElementById("broken cookie").classList.remove("invisible");
+            document.getElementById("misfortune").textContent = FORTUNES[Math.floor(Math.random() * FORTUNES.length)];
         } else {
             document.getElementById("broken cookie").classList.add("invisible");
             document.getElementById("cookie").classList.remove("invisible");
