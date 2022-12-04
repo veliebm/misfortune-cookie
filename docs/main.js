c037c89479a1e3193e0fba2b4cc9ece9c02c804b
veliebm <veliebm@gmail.com>
2022-12-04 12:00:00 -0600

    Refactor to break into more functions

--- docs/main.js
+++ docs/main.js
@@ -1,10 +1,12 @@
+// Script to make and break misfortune cookies.
+
 const FORTUNES = [
     "Abandon all hope immediately.",
     "Misfortune favors you a whole lot.",
     "By the time you're reading this, it's already too late.",
 ];
 
-var cookie_broken = false;
+let cookie_broken = false;
 
 document.onkeydown = function (e) {
     e = e || window.event;
@@ -19,17 +21,27 @@ document.onkeyup = function (e) {
     e = e || window.event;
     if (e.key == " ") {
         if (!cookie_broken) {
-            document.getElementById("cookie").classList.remove("shake");
-            document.getElementById("cookie").classList.add("invisible");
-            cookie_broken = true;
-            document.getElementById("broken cookie").classList.remove("invisible");
-            document.getElementById("misfortune").textContent = FORTUNES[Math.floor(Math.random() * FORTUNES.length)];
-            document.getElementById("message to user").textContent = "Your misfortune has been granted.";
+            break_cookie();
         } else {
-            document.getElementById("broken cookie").classList.add("invisible");
-            document.getElementById("cookie").classList.remove("invisible");
-            cookie_broken = false;
-            document.getElementById("message to user").textContent = "Press the spacebar to break me open.";
+            get_new_cookie();
         }
     }
 };
+
+// Deletes unbroken cookie and replaces it with broken cookie.
+function break_cookie() {
+    document.getElementById("cookie").classList.remove("shake");
+    document.getElementById("cookie").classList.add("invisible");
+    cookie_broken = true;
+    document.getElementById("broken cookie").classList.remove("invisible");
+    document.getElementById("misfortune").textContent = FORTUNES[Math.floor(Math.random() * FORTUNES.length)];
+    document.getElementById("message to user").textContent = "Your misfortune has been granted.";
+}
+
+// Deletes broken cookie and replaces it with new cookie.
+function get_new_cookie() {
+    document.getElementById("broken cookie").classList.add("invisible");
+    document.getElementById("cookie").classList.remove("invisible");
+    cookie_broken = false;
+    document.getElementById("message to user").textContent = "Press the spacebar to break me open.";
+}
