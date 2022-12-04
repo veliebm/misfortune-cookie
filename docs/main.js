3dd68eddd6aa6066a55e64e70874ee7b91f76fc2
veliebm <veliebm@gmail.com>
2022-12-04 11:17:13 -0600

    Add ability to break the cookie with the spacebar.

--- docs/main.js
+++ docs/main.js
@@ -1,14 +1,26 @@
+var cookie_broken = false;
+
 document.onkeydown = function (e) {
     e = e || window.event;
-    console.log("Keypress detected");
     if (e.key == " ") {
-        document.getElementById("cookie").classList.add("shake");
+        if (!cookie_broken) {
+            document.getElementById("cookie").classList.add("shake");
+        }
     }
 };
 
 document.onkeyup = function (e) {
     e = e || window.event;
     if (e.key == " ") {
-        document.getElementById("cookie").classList.remove("shake");
+        if (!cookie_broken) {
+            document.getElementById("cookie").classList.remove("shake");
+            document.getElementById("cookie").classList.add("invisible");
+            cookie_broken = true;
+            document.getElementById("broken cookie").classList.remove("invisible");
+        } else {
+            document.getElementById("broken cookie").classList.add("invisible");
+            document.getElementById("cookie").classList.remove("invisible");
+            cookie_broken = false;
+        }
     }
 };
